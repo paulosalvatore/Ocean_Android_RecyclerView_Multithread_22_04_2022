59e42e60054fd4930020e5a07613b8836aa1f23a
Paulo Salvatore <salvatoregames@gmail.com>
2022-04-22 11:08:55 -0300

    Implementação do `ViewHolder`, `onCreateViewHolder` e `onBindViewHolder`

--- app/src/main/java/com/oceanbrasil/ocean_android_recyclerview_multithread_22_04_2022/PokemonAdapter.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_recyclerview_multithread_22_04_2022/PokemonAdapter.kt
@@ -1,18 +1,37 @@
 package com.oceanbrasil.ocean_android_recyclerview_multithread_22_04_2022
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class PokemonAdapter(val itens: List<Pokemon>) : RecyclerView.Adapter<PokemonAdapter.ViewHolder>() {
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bindView(item: Pokemon) = with(itemView) {
+            val tvNome = findViewById<TextView>(R.id.tvNome)
+            val ivPokemon = findViewById<ImageView>(R.id.ivPokemon)
+
+            tvNome.text = item.name
+
+            val urlImagem = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png"
+            Glide.with(this).load(urlImagem).into(ivPokemon)
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val layoutInflater = LayoutInflater.from(parent.context)
+
+        val view = layoutInflater.inflate(R.layout.pokemon_item, parent, false)
+
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = itens[position]
+        holder.bindView(item)
     }
 
     override fun getItemCount(): Int {
