6b9565791fa0d28c25b051e1e739de4e9a24eefe
Paulo Salvatore <salvatoregames@gmail.com>
2022-04-22 11:56:15 -0300

    Exibindo todos os pokémon

--- app/src/main/java/com/oceanbrasil/ocean_android_recyclerview_multithread_22_04_2022/PokemonService.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_recyclerview_multithread_22_04_2022/PokemonService.kt
@@ -1,10 +1,9 @@
 package com.oceanbrasil.ocean_android_recyclerview_multithread_22_04_2022
 
-import com.oceanbrasil.ocean_android_recyclerview_multithread_22_04_2022.PokemonApiResult
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface PokemonService {
-    @GET("pokemon")
+    @GET("pokemon?limit=100000&offset=0")
     fun listarPokemon(): Call<PokemonApiResult>
 }
